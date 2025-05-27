e92fb62e9b61c1e8fe2ce4f4330e96908ac89bf5
Rukieya Ainoo <a012702h@student.staffs.ac.uk>
2025-05-27 10:16:19 +0100

    frontend and backend integrated

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 var cors = require('cors')
 const routes = require('./routes/home');
 const app = express();
+app.use(cors({origin: '*'}));
 app.use(routes);
-app.options('*', cors())
 const port = 3330;
 
 app.listen(port, () => {
---
+++ frontend/src/components/Learn.vue
@@ -0,0 +1,30 @@
+<script>
+   import axios from 'axios'
+
+export default{
+ data() {
+    return {
+      data: "",
+    };
+  },
+  methods: {
+    displayData(){
+     axios.get("http://localhost:3330/")
+     .then((res) => {this.data = res.data})
+     .catch((err) => console.log(err.message))
+   }
+  }
+   }
+</script>
+
+<template>
+  
+  <main>
+    <button @click="displayData">Click me</button>
+    {{ data }}
+  </main>
+</template>
+
+<style scoped>
+
+</style>
